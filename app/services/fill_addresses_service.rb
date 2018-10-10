59b6de463d928ca0ccd1812655b63fa76a916bad
elina <elinakolod@gmail.com>
2018-10-10 14:00:41 +0300

    save unfinishedorder's adress info in checkout for returning customer

--- app/services/fill_addresses_service.rb
+++ app/services/fill_addresses_service.rb
@@ -1,11 +1,9 @@
 class FillAddressesService
   class << self
     def call(order:, order_params:)
-      if order.may_fill_address?
-        order.assign_attributes(order_params)
-        order.shipping_address.assign_attributes(order_params[:billing_address_attributes]) if order_params[:billing_flag].eql?('1')
-        order.fill_address! if order.save
-      end
+      order.assign_attributes(order_params)
+      order.shipping_address.assign_attributes(order_params[:billing_address_attributes]) if order_params[:billing_flag].eql?('1')
+      order.fill_address! if order.may_fill_address? && order.save
     end
   end
 end
