126a64186aaa8481a62f3ac25f2901d9d192d638
elina <elinakolod@gmail.com>
2018-11-24 19:03:14 +0200

    send email only after registration by fb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-      ApplicationMailer.welcome_email(@user, @user.password).deliver
+      ApplicationMailer.welcome_email(@user, @user.password).deliver if @user.password
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
       redirect_to signup_users_url
